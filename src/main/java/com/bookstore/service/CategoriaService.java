c3b20d7841dfa7f359e63bc9b4be2856b2b2e8f3
Alisson7Neres <alisson.neres@hotmail.com>
2021-12-15 11:04:27 -0300

    post/update/delete

--- src/main/java/com/bookstore/service/CategoriaService.java
+++ src/main/java/com/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bookstore.domain.Categoria;
+import com.bookstore.dtos.CategoriaDTO;
 import com.bookstore.exceptions.ObjectNotFoundException;
 import com.bookstore.repository.CategoriaRepository;
 
@@ -15,14 +16,32 @@ public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElseThrow(( () -> new ObjectNotFoundException(
+		return obj.orElseThrow((() -> new ObjectNotFoundException(
 				"Objeto não encontrado! id: " + id + ", tipo: " + Categoria.class.getName())));
 	}
-	
+
 	public List<Categoria> findAll() {
 		return repository.findAll();
+	}
+
+	public Categoria create(Categoria obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
+
+		return repository.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		findById(id);
+		 repository.deleteById(id);
 	}
 }
